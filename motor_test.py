b0145b986b26b9334ecb47d9d4e77a479f574a7e
matteo-martinelli <wmatte2005@gmail.com>
2023-03-03 16:40:25 +0100

    Changed the "blocking" value in the .cycleloop fun

--- motor_test.py
+++ motor_test.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 import revpimodio2
-from multiprocessing import MultiProcessing
 import time
 
 
 class Test(object):
     def __init__(self):
-        self.state = False
-        self.pin = 3
-    
         self.rpi = revpimodio2.RevPiModIO(autorefresh=True)
-        self.rpi.handlesignalend(self.programend) 
-        #self.multiprocessing = MultiProcessing(1)
-        #print("Go into cycleloop")
-        #self.rpi.cycleloop(self.cycleprogram)
-        #print("Left the cycleloop")
+        self.rpi.handlesignalend(self.rpi_cleanup)
+        
+        self.pin = 3
+        self.state = self.rpi.io['O_' + str(self.pin)].value
+
+        print("Go into cycleloop")
+        self.rpi.cycleloop(self.cycleprogram, blocking=False)
+        print("Left the cycleloop")
     
     def set_state(self, value: bool):
         self.state = value
@@ -31,12 +30,14 @@ class Test(object):
         
     def rpi_cleanup(self):
         """This function does cleanup work before the end of the program."""
-        self.rpi.core.a1.green.value = False
+        self.rpi.core.a1green.value = False
         self.rpi.io['O_' + str(self.pin)].value = False
 
 if __name__ == '__main__':
     motor_saw = Test()
+    print('Setting the state to True')
     motor_saw.set_state(True)
     time.sleep(5)
+    print('Setting the state to False')
     motor_saw.set_state(False)
     
